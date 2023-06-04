8535db930bc8d817e52bc67cf36f62a8388b0f17
Kenshi <james85529@gmail.com>
2023-06-04 14:25:27 +0800

    generate QR and text file from url

--- Backend/2.4 QR Code Project/index.js
+++ Backend/2.4 QR Code Project/index.js
@@ -5,15 +5,39 @@
 */
 import inquirer from "inquirer";
 import qr from "qr-image";
+import fs from "fs";
 
-var url = "";
+var url;
 
 inquirer
-  .prompt(["Enter url you want to convert to QR code: "])
+  .prompt([
+    {
+      type: "input",
+      name: "url",
+      message: "Enter url you want to convert to QR code:",
+    },
+  ])
   .then((answer) => {
-    url = answer;
-    console.log(url);
+    url = answer.url;
+    generateQR(url);
+    generateTxt(url);
+    console.log(answer.url);
   })
   .catch((err) => {
     console.error(err);
   });
+
+/**
+ *  Generate QR code from url */
+function generateQR(url) {
+  var qr_png = qr.image(url);
+  qr_png.pipe(fs.createWriteStream("qr-code.png"));
+}
+
+/**
+ * Generate TXT file from url */
+function generateTxt(url) {
+  fs.writeFile("URL.txt", url, (err) => {
+    if (err) throw err;
+  });
+}
